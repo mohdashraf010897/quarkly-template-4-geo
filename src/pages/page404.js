4c1e119969a2df1ec0a56f60e232cb8434611f33
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-01-07 22:33:05 +0530

    removed extra script

--- src/pages/page404.js
+++ src/pages/page404.js
@@ -18,7 +18,6 @@ export default (() => {
 			<style place={"endOfHead"} rawKey={"61d5d704637b4d001f7bb576"}>
 				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
 			</style>
-			<script type={"text/javascript"} src={"https://maps.google.com/maps/api/js?key=AIzaSyA9JzjtHeXg_C_hh_GdTBdLxREWdj3nsOU&v=weekly&libraries=places"} place={"endOfHead"} rawKey={"61d861698410d2ff4422d52f"} />
 		</RawHtml>
 	</Theme>;
 });
